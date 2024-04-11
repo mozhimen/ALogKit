66d12168c5264040487541e40362a473404df5b2
Kolin Zhao & Mozhimen <58215586+mozhimen@users.noreply.github.com>
2024-04-12 00:44:19 +0800

    0412

--- logk/src/main/java/com/mozhimen/logk/bases/BaseLogKRecord.kt
+++ logk/src/main/java/com/mozhimen/logk/bases/BaseLogKRecord.kt
@@ -2,7 +2,7 @@ package com.mozhimen.logk.bases
 
 import com.mozhimen.basick.elemk.java.util.cons.CDateFormat
 import com.mozhimen.basick.utilk.java.util.UtilKDateWrapper
-import com.mozhimen.basick.utilk.kotlin.intLogPriority2strLogSimple
+import com.mozhimen.basick.utilk.kotlin.intLogPriority2strLogPriority_ofSimple
 
 /**
  * @ClassName LogKMo
@@ -17,5 +17,5 @@ open class BaseLogKRecord(var timeMillis: Long, var priority: Int, var tag: Stri
         getFlattened() + msg
 
     open fun getFlattened(): String =
-        "${UtilKDateWrapper.longDate2strDate(timeMillis, CDateFormat.yyyy_MM_dd_HH_mm_ss)} | Level: ${priority.intLogPriority2strLogSimple()} | Tag: $tag : "
+        "${UtilKDateWrapper.longDate2strDate(timeMillis, CDateFormat.yyyy_MM_dd_HH_mm_ss)} | Level: ${priority.intLogPriority2strLogPriority_ofSimple()} | Tag: $tag : "
 }
